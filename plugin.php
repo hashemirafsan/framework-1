a5375c2dd3a2ead9877fa5856f2966e9c02607d9
Sheikh Heera <heerasheikh@ymail.com>
2017-11-12 22:58:00 +0600

    modified plugin.php

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
 <?php
 
 return array(
-	'namespace' => 'Glue',
-	'plugin_name' => 'The Glue',
-	'plugin_slug' => 'glue',
-	'plugin_uri' => 'http://glue.com',
-	'plugin_version' => '1.0.0',
-	'author_name' => 'Sheikh Heera',
-	'author_email' => 'heera.sheikh77@gmail.com',
-	'author_uri' => 'http://heera.it',
+	'namespace' => null,
+	'plugin_name' => null,
+	'plugin_slug' => null,
+	'plugin_uri' => null,
+	'plugin_version' => null,
+	'author_name' => null,
+	'author_email' => null,
+	'author_uri' => null,
 );
